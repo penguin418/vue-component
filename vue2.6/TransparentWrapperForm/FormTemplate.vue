c2effcaab1e16569835edfca4346fdc7315b3a69
penguin418 <penguin418@users.noreply.github.com>
2022-06-08 03:58:00 +0900

    Create FormTemplate.vue

---
+++ vue2.6/TransparentWrapperForm/FormTemplate.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="component" :value="value" @update="update" />
+</template>
+
+<script>
+import FormTwo from "./FormTwo";
+import FormOne from "./FormOne";
+export default {
+  components: { FormTwo, FormOne },
+  props: {
+    value: { type: Object, default: () => ({}) },
+  },
+  computed: {
+    component() {
+      if (this.value.tp === "facebook") return "FormOne";
+      else return "FormTwo";
+    },
+  },
+  methods: {
+    update(value) {
+      this.$emit("input", value);
+    },
+  },
+};
+</script>
+
+<style></style>
