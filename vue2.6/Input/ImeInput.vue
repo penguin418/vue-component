37074fc19707a54d0e113a8e584b325e64c19aee
penguin418 <penguin418@users.noreply.github.com>
2022-06-08 03:49:21 +0900

    Create ImeInput.vue

---
+++ vue2.6/Input/ImeInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <input :value="value" @input="input" v-bind="$attrs" v-on="listeners" />
+</template>
+
+<script>
+export default {
+  name: "ImeInput",
+  props: { value: { type: String, default: "" } },
+  computed: {
+    listeners() {
+      const { input, ...listeners } = this.$listeners;
+      return listeners;
+    },
+  },
+  methods: {
+    input(event) {
+      this.$emit("input", event.target.value);
+    },
+  },
+};
+</script>
+
+<style></style>
